80bf7309ba32da80f41fc4318876323e461ee2b4
dnwdark <Artec40@gmail.com>
2020-10-27 00:00:40 +0500

    Для роутинга в Angular встроен app-routing.

--- app-angular/src/app/app-routing.module.ts
+++ app-angular/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {InfoApiComponent} from './informer/info-api/info-api.component';
+import {InfoAppComponent} from './informer/info-app/info-app.component';
+import {TitleComponent} from './Informer/title/title.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'api', component: InfoApiComponent},
+  {path: 'app', component: InfoAppComponent},
+  {path: '**', component: TitleComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
---
+++ app-angular/src/app/informer/info-api/info-api.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-info-api',
+  templateUrl: './info-api.component.html',
+  styleUrls: ['./info-api.component.scss']
+})
+export class InfoApiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
